3c816ae3c2e7c6a65272d82afa352ddfc98434d6
SpringZz <wang90944@gmail.com>
2022-11-11 23:17:25 +0800

    添加router

---
+++ src/routes/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Redirect } from 'react-router';
+import Home from '../application/Home';
+import Recommend from '../application/Recommend';
+import Singers from '../application/Singers';
+import Rank from '../application/Rank';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    path: '/',
+    component: Home,
+    routes: [
+      {
+        path: '/',
+        exact: true,
+        render: () => <Redirect to={'/recommend'} />,
+      },
+      {
+        path: '/recommend',
+        component: Recommend,
+      },
+      {
+        path: '/singers',
+        component: Singers,
+      },
+      {
+        path: '/rank',
+        component: Rank,
+      },
+    ],
+  },
+];
