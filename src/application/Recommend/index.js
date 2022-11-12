858e40fe5f55e85a036e6b1ebacfa6b99085dab2
SpringZz <wang90944@gmail.com>
2022-11-12 23:02:25 +0800

    轮播组件开发，尚待完善

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Slider from '../../components/slider'
 
 function Recommend(props) {
-  return <div>Recommend ffasdf</div>;
+
+  // mock数据
+  const bannerList =[1,2,3,4].map(item=>{
+    return { imageUrl:"http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg" }
+  });
+
+  return <Slider bannerList={bannerList}></Slider>
 }
 
 export default React.memo(Recommend);
