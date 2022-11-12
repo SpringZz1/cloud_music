e7859693ac8e82a5cef1dcceb65a1b2ccad4e5e1
SpringZz <wang90944@gmail.com>
2022-11-12 17:18:33 +0800

    路由修改

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,31 +1,34 @@
 import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 import Home from '../application/Home';
 import Recommend from '../application/Recommend';
 import Singers from '../application/Singers';
 import Rank from '../application/Rank';
 
-const router = createBrowserRouter( [
+const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
     children: [
       // 默认跳转到推荐列表
       {
-        // index: true,
-        path: '/recommend',
-        element: <Recommend />,
+        index: true,
+        element: <Navigate to="/recommend" />,
       },
       {
         path: '/singers',
-        element: <Singers />
+        element: <Singers />,
       },
       {
         path: 'rank',
-        element: <Rank />
-      }
-    ]
-  }
-])
+        element: <Rank />,
+      },
+      {
+        path: '/recommend',
+        element: <Recommend />,
+      },
+    ],
+  },
+]);
 
 export default router;
