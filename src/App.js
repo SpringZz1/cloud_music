7a1edc2d4edae3349fb72912d0bb61296c823134
SpringZz <wang90944@gmail.com>
2022-11-12 17:27:54 +0800

    添加redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { GlobalStyle } from './style';
 import { IconStyle } from './assets/iconfont/iconfont';
 import router from './routes';
-import { HashRouter, Route, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
+import store from './store';
+// import reducer from './store/reducer';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle></GlobalStyle>
       <IconStyle></IconStyle>
       <RouterProvider router={router}></RouterProvider>
       {/* <div className="App">
         <i className="iconfont">&#xe62b;</i>
       </div> */}
-    </>
+    </Provider>
   );
 }
 
---
+++ src/store/reducer.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux-immutable";
+
+export default combineReducers({
+  // 等到开发具体模块的时候添加reducer
+})
