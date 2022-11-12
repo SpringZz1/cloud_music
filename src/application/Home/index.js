e40f79e9c04f954a7b2f10ca527724d5d28321bd
SpringZz <wang90944@gmail.com>
2022-11-12 17:17:47 +0800

    主页面标签栏

--- src/application/Home/index.js
+++ src/application/Home/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { Outlet } from 'react-router'
+import { Outlet } from 'react-router';
+import { Top, Tab, TabItem } from './style';
+import { NavLink } from 'react-router-dom';
 
 function Home(props) {
-  return <div>
-    <h1>Home</h1>
-    <Outlet />
-  </div>;
+  return (
+    <div>
+      <Top>
+        <span className="iconfonr menu">&#xe65c;</span>
+        <span className="title">WebApp</span>
+        <span className="iconfont search">&#xe62b;</span>
+      </Top>
+      <Tab>
+        <NavLink to="/" activeclassname="selected">
+          <TabItem>
+            <span>推荐</span>
+          </TabItem>
+        </NavLink>
+        <NavLink to="/singers" activeclassname="selected">
+          <TabItem>
+            <span>歌手</span>
+          </TabItem>
+        </NavLink>
+        <NavLink to="/rank" activeclassname="selected">
+          <TabItem>
+            <span>排行</span>
+          </TabItem>
+        </NavLink>
+      </Tab>
+      <Outlet />
+    </div>
+  );
 }
 
 export default React.memo(Home);
