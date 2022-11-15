fe63cdfeeeb49f6ea83e836cef51ffcd152694b5
SpringZz <wang90944@gmail.com>
2022-11-15 16:16:06 +0800

    存放 initialState 和 reducer 函数

---
+++ src/application/Recommend/store/reducer.js
@@ -0,0 +1,21 @@
+import * as actionTypes from './constants';
+import { fromJS } from 'immutable'; // 这里使用到fromJS把JS数据转化为immutable数据结构
+
+const defaultState = fromJS({
+  bannerList: [],
+  recommendList: [],
+});
+
+// 因为是使用immutable数据结构，所以必须使用set来改变新状态，同时取状态使用get方法
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = defaultState, action) => {
+  switch (action.type) {
+    case actionTypes.CHANGE_BANNER:
+      return state.set('bannerList', action.data);
+    case actionTypes.CHANGE_RECOMMEND_LIST:
+      return state.set('recommendList', action.data);
+    default:
+      return state;
+  }
+};
