ca8b4832e5e7eac161db90f9c1de94fe8d321209
SpringZz <wang90944@gmail.com>
2022-11-15 16:15:28 +0800

    从以前的mock数据变为请求端口数据渲染

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,38 +1,77 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Slider from '../../components/slider';
+import { connect } from 'react-redux';
+import * as actionTypes from './store/actionCreators';
 import RecommendList from '../../components/list';
 import { Content } from './style';
 import Scroll from '../../baseUI/scroll';
 
 function Recommend(props) {
-  // mock数据
-  const bannerList = [1, 2, 3, 4].map((item) => {
-    return {
-      imageUrl:
-        'http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg',
-    };
-  });
+  const { bannerList, recommendList } = props;
+  const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
 
-  const recommendList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => {
-    return {
-      id: 1,
-      picUrl:
-        'https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg',
-      playCount: 17171122,
-      name: '朴树、许巍、李健、郑钧、老狼、赵雷',
-    };
-  });
+  useEffect(() => {
+    getBannerDataDispatch();
+    getRecommendListDataDispatch();
+    //eslint-disable-next-line
+  }, []);
+
+  const bannerListJS = bannerList ? bannerList.toJS() : [];
+  const recommendListJS = recommendList ? recommendList.toJS() : [];
+
+  // mock数据
+  // const bannerList = [1, 2, 3, 4].map((item) => {
+  //   return {
+  //     imageUrl:
+  //       'http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg',
+  //   };
+  // });
+
+  // const recommendList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => {
+  //   return {
+  //     id: 1,
+  //     picUrl:
+  //       'https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg',
+  //     playCount: 17171122,
+  //     name: '朴树、许巍、李健、郑钧、老狼、赵雷',
+  //   };
+  // });
 
   return (
     <Content>
-      <Scroll className='list'>
+      <Scroll className="list">
         <div>
-          <Slider bannerList={bannerList}></Slider>
-          <RecommendList recommendList={recommendList}></RecommendList>
+          <Slider bannerList={bannerListJS}></Slider>
+          <RecommendList recommendList={recommendListJS}></RecommendList>
         </div>
       </Scroll>
     </Content>
   );
 }
 
-export default React.memo(Recommend);
+// 映射Redux全局的state到组件的props上
+const mapStateToProps = (state) => ({
+  // 不要在这里将数据 toJS
+  // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
+  bannerList: state.getIn(['recommend', 'bannerList']),
+  recommendList: state.getIn(['recommend', 'recommendList']),
+});
+
+// 映射dispatch到props上
+const mapDispathToProps = (dispatch) => {
+  return {
+    getBannerDataDispatch() {
+      dispatch(actionTypes.getBannerList());
+    },
+    getRecommendListDataDispatch() {
+      dispatch(actionTypes.getRecommendList());
+    },
+  };
+};
+
+// 将ui组件包装成容器组件
+export default connect(
+  mapStateToProps,
+  mapDispathToProps
+)(React.memo(Recommend));
+// export default React.memo(Recommend);
