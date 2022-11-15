b5bccc72621c1e31ecc3d40aa6fe0d4e8b0fc40a
SpringZz <wang90944@gmail.com>
2022-11-15 16:13:10 +0800

    注册推荐功能的reducer到全局store

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,8 @@
-import { combineReducers } from "redux-immutable";
+import { combineReducers } from 'redux-immutable';
+import { reducer as recommendReducer } from '../application/Recommend/store/index'; // recommend模块的reducer
 
+// 将所有reducer注册到全局store
 export default combineReducers({
   // 等到开发具体模块的时候添加reducer
-})
+  recommend: recommendReducer,
+});
