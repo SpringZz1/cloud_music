d83a0dff360286f2bba0c38b944f73954acd9c0c
SpringZz <wang90944@gmail.com>
2022-11-16 18:00:10 +0800

    歌手reducer挂载到全局

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux-immutable';
 import { reducer as recommendReducer } from '../application/Recommend/store/index'; // recommend模块的reducer
+import { reducer as singerReducer } from '../application/Singers/store/index'; // singer模块的reducer
 
 // 将所有reducer注册到全局store
 export default combineReducers({
   // 等到开发具体模块的时候添加reducer
   recommend: recommendReducer,
+  singers: singerReducer,
 });
