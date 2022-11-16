8504c8823305be94c19df151097f825cec583cc3
SpringZz <wang90944@gmail.com>
2022-11-16 18:01:14 +0800

    singer的redux

--- src/application/Singers/store/reducer.js
+++ src/application/Singers/store/reducer.js
@@ -1,4 +1,5 @@
 import { fromJS } from 'immutable';
+import * as actionTypes from './constants';
 
 const defaultState = fromJS({
   singerList: [],
@@ -7,3 +8,21 @@ const defaultState = fromJS({
   pullDownLoading: false, // 控制下拉加载动画
   pageCount: 0, // 当前页数, 接下来要做一个分页功能
 });
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = defaultState, action) => {
+  switch (action.type) {
+    case actionTypes.CHANGE_SINGER_LIST:
+      return state.set('singerList', action.data);
+    case actionTypes.CHANGE_PAGE_COUNT:
+      return state.set('pageCount', action.data);
+    case actionTypes.CHANGE_ENTER_LOADING:
+      return state.set('enterLoading', action.data);
+    case actionTypes.CHANGE_PULLUP_LOADING:
+      return state.set('pullUpLoading', action.data);
+    case actionTypes.CHANGE_PULLDOWN_LOADING:
+      return state.set('pullDownLoading', action.data);
+    default:
+      return state;
+  }
+};
