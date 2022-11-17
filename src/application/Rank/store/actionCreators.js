df476e71410fa81b6a0dbf26ffff3e0576411a1f
SpringZz <wang90944@gmail.com>
2022-11-17 16:59:17 +0800

    rank的redux

---
+++ src/application/Rank/store/actionCreators.js
@@ -0,0 +1,24 @@
+import { fromJS } from 'immutable';
+import { getRankListRequest } from '../../../api/request';
+import { CHANGE_RANK_LIST, CHANGE_LOADING } from './constants';
+
+const changeRankList = (data) => ({
+  type: CHANGE_RANK_LIST,
+  data: fromJS(data),
+});
+
+const changeLoading = (data) => ({
+  type: CHANGE_LOADING,
+  data: data,
+});
+
+export const getRankList = () => {
+  return (dispatch) => {
+    getRankListRequest().then((data) => {
+      let list = data && data.list;
+      console.log(list);
+      dispatch(changeRankList(list));
+      dispatch(changeLoading(false));
+    });
+  };
+};
