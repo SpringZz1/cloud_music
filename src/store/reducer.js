013b78345903aad29d6cb7f361d08ebc9b178c0d
SpringZz <wang90944@gmail.com>
2022-11-23 17:42:29 +0800

    全局挂载歌手详情信息的redux

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux-immutable';
 import { reducer as recommendReducer } from '../application/Recommend/store/index'; // recommend模块的reducer
-import { reducer as singerReducer } from '../application/Singers/store/index'; // singer模块的reducer
+import { reducer as singerReducer } from '../application/Singers/store/index'; // singers模块的reducer
 import { reducer as rankReducer } from '../application/Rank/store/index'; // rank模块的reducer
 import { reducer as albumReducer } from '../application/Album/store/index'; // album模块的reducer
+import { reducer as singerInfoReducer } from '../application/Singer/store/index'; // singer模块的reducer
 
 // 将所有reducer注册到全局store
 export default combineReducers({
@@ -11,4 +12,5 @@ export default combineReducers({
   singers: singerReducer,
   rank: rankReducer,
   album: albumReducer,
+  singerInfo: singerInfoReducer
 });
