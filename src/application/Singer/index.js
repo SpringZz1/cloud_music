ffac91ebca020f75037a17eaec52af6b156bae02
SpringZz <wang90944@gmail.com>
2022-11-23 17:42:09 +0800

    使用真实请求的数据渲染歌手详情页面

--- src/application/Singer/index.js
+++ src/application/Singer/index.js
@@ -1,15 +1,24 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import { Container } from './style';
-import { useNavigate } from 'react-router';
+import { HEADER_HEIGHT } from './../../api/config';
 import { ImgWrapper, CollectButton, SongListWrapper, BgLayer } from './style';
 import Header from '../../baseUI/header/index';
 import Scroll from '../../baseUI/scroll/index';
 import SongsList from '../SongsList';
+import { connect } from 'react-redux';
+import Loading from './../../baseUI/loading/index';
+import { getSingerInfo, changeEnterLoading } from './store/actionCreators';
+import { useParams, useNavigate } from 'react-router';
 
 function Singer(props) {
+  const initialHeight = useRef(0);
   const [showStatus, setShowStatus] = useState(true);
-  const navigate = useNavigate();
+
+  const { artist: immutableArtist, songs: immutableSongs, loading } = props;
+
+  const { getSingerDataDispatch } = props;
+
   const collectButton = useRef();
   const imageWrapper = useRef();
   const songScrollWrapper = useRef();
@@ -17,81 +26,119 @@ function Singer(props) {
   const header = useRef();
   const layer = useRef();
 
-  // 图片初始高度
-  const initialHeight = useRef(0);
-
-  // 往上偏移的尺寸，露出圆角
+  //往上偏移的尺寸，露出圆角
   const OFFSET = 5;
 
+  const navigate = useNavigate();
+  const id = useParams();
+
   useEffect(() => {
+    getSingerDataDispatch(id.id);
+    // console.log(id.id);
     let h = imageWrapper.current.offsetHeight;
-    songScrollWrapper.current.style.top = `${h - OFFSET}px`;
     initialHeight.current = h;
-    // 把遮罩先放在下面，以裹住歌曲列表
+    songScrollWrapper.current.style.top = `${h - OFFSET}px`;
+    //把遮罩先放在下面，以裹住歌曲列表
     layer.current.style.top = `${h - OFFSET}px`;
     songScroll.current.refresh();
     // eslint-disable-next-line
+  }, []);
+
+  const artist = immutableArtist.toJS();
+  const songs = immutableSongs.toJS();
+
+  const handleScroll = useCallback((pos) => {
+    let height = initialHeight.current;
+    const newY = pos.y;
+    const imageDOM = imageWrapper.current;
+    const buttonDOM = collectButton.current;
+    const headerDOM = header.current;
+    const layerDOM = layer.current;
+    const minScrollY = -(height - OFFSET) + HEADER_HEIGHT;
+
+    //指的是滑动距离占图片高度的百分比
+    const percent = Math.abs(newY / height);
+
+    if (newY > 0) {
+      imageDOM.style['transform'] = `scale(${1 + percent})`;
+      buttonDOM.style['transform'] = `translate3d(0, ${newY}px, 0)`;
+      layerDOM.style.top = `${height - OFFSET + newY}px`;
+    } else if (newY >= minScrollY) {
+      layerDOM.style.top = `${height - OFFSET - Math.abs(newY)}px`;
+      //这时候保证遮罩的层叠优先级比图片高，不至于被图片挡住
+      layerDOM.style.zIndex = 1;
+      imageDOM.style.paddingTop = '75%';
+      imageDOM.style.height = 0;
+      imageDOM.style.zIndex = -1;
+      //按钮跟着移动且渐渐变透明
+      buttonDOM.style['transform'] = `translate3d(0, ${newY}px, 0)`;
+      buttonDOM.style['opacity'] = `${1 - percent * 2}`;
+    } else if (newY < minScrollY) {
+      //往上滑动，但是超过Header部分
+      layerDOM.style.top = `${HEADER_HEIGHT - OFFSET}px`;
+      layerDOM.style.zIndex = 1;
+      //防止溢出的歌单内容遮住Header
+      headerDOM.style.zIndex = 100;
+      //此时图片高度与Header一致
+      imageDOM.style.height = `${HEADER_HEIGHT}px`;
+      imageDOM.style.paddingTop = 0;
+      imageDOM.style.zIndex = 99;
+    }
   }, []);
 
   const setShowStatusFalse = useCallback(() => {
     setShowStatus(false);
   }, []);
 
-  const artist = {
-    picUrl:
-      'https://p2.music.126.net/W__FCWFiyq0JdPtuLJoZVQ==/109951163765026271.jpg',
-    name: '薛之谦',
-    hotSongs: [
-      {
-        name: '我好像在哪见过你',
-        ar: [{ name: '薛之谦' }],
-        al: {
-          name: '薛之谦专辑',
-        },
-      },
-      {
-        name: '我好像在哪见过你',
-        ar: [{ name: '薛之谦' }],
-        al: {
-          name: '薛之谦专辑',
-        },
-      },
-    ],
-  };
-
   return (
     <CSSTransition
       in={showStatus}
       timeout={300}
-      classNames={{
-        exitActive: 'animate__animated animate__backOutDown',
-      }}
+      classNames="fly"
       appear={true}
-      unmountOnExit={true}
+      unmountOnExit
       onExited={() => navigate(-1)}
     >
       <Container>
         <Header
-          title={'头部'}
           handleClick={setShowStatusFalse}
+          title={artist.name}
           ref={header}
         ></Header>
-        <ImgWrapper bgUrl={artist.picUrl} ref={imageWrapper}>
+        <ImgWrapper ref={imageWrapper} bgUrl={artist.picUrl}>
           <div className="filter"></div>
         </ImgWrapper>
         <CollectButton ref={collectButton}>
           <i className="iconfont">&#xe62d;</i>
-          <span className="text"> 收藏 </span>
+          <span className="text">收藏</span>
         </CollectButton>
         <BgLayer ref={layer}></BgLayer>
         <SongListWrapper ref={songScrollWrapper}>
-          <Scroll ref={songScroll}>
-            <SongsList songs={artist.hotSongs} showCollect={false}></SongsList>
+          <Scroll ref={songScroll} onScroll={handleScroll}>
+            <SongsList songs={songs} showCollect={false}></SongsList>
           </Scroll>
         </SongListWrapper>
+        {loading ? <Loading></Loading> : null}
       </Container>
     </CSSTransition>
   );
 }
 
-export default Singer;
+// 映射Redux全局的state到组件的props上
+const mapStateToProps = (state) => ({
+  artist: state.getIn(['singerInfo', 'artist']),
+  songs: state.getIn(['singerInfo', 'songsOfArtist']),
+  loading: state.getIn(['singerInfo', 'loading']),
+});
+// 映射dispatch到props上
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getSingerDataDispatch(id) {
+      dispatch(changeEnterLoading(true));
+      dispatch(getSingerInfo(id));
+    },
+  };
+};
+
+// 将ui组件包装成容器组件
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Singer));
