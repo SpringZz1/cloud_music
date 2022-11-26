7d16ea74b5ca27c481f101f2d56926dc210eac3d
SpringZz <wang90944@gmail.com>
2022-11-26 15:51:04 +0800

    添加随机算法和寻找歌曲索引以供歌曲模式切换

--- src/api/utils.js
+++ src/api/utils.js
@@ -85,3 +85,29 @@ export const formatPlayTime = (interval) => {
   const second = (interval % 60).toString().padStart(2, '0');
   return `${minute}:${second}`;
 };
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// 随机算法
+export const shuffle = (arr) => {
+  let newArr = [];
+  arr.forEach((item) => {
+    newArr.push(item);
+  });
+  for (let i = 0; i < newArr.length; i++) {
+    let j = getRandomInt(0, i);
+    let t = newArr[j];
+    newArr[i] = newArr[j];
+    newArr[j] = t;
+  }
+  return newArr;
+};
+
+// 找到当前的歌曲索引
+export const findIndex = (song, list) => {
+  return list.findIndex((item) => {
+    return song.id === item.id;
+  });
+};
