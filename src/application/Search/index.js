790e1d48ac309fb33874611d7c860c825fbaf013
SpringZz <wang90944@gmail.com>
2022-12-02 22:10:08 +0800

    添加search-box的具体逻辑函数

--- src/application/Search/index.js
+++ src/application/Search/index.js
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import { Container } from './style';
 import { useNavigate } from 'react-router';
+import SearchBox from './../../baseUI/search-box/index';
 
 function Search(props) {
   // 控制动画
   const [show, setShow] = useState(false);
+  const [query, setQuery] = useState('');
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -14,6 +16,15 @@ function Search(props) {
 
   const setShowFalse = () => {
     console.log('setShowFalse function');
+  };
+
+  // 由于是传给子组件的方法, 尽量使用useCallBack包裹, 使得在依赖未改变, 始终给子组件传递给的是相同的引用
+  const searchBack = useCallback(() => {
+    setShow(false);
+  }, []);
+
+  const handleQuery = (q) => {
+    setQuery(q);
   };
 
   return (
@@ -26,7 +37,10 @@ function Search(props) {
       onExit={() => navigate(-1)}
     >
       <Container>
-        <div onClick={() => setShowFalse}>返回</div>
+        <div className="search_box_wrapper">
+          <SearchBox back={searchBack} newQuery={query} handleQuery={handleQuery}></SearchBox>
+        </div>
+        {/* <div onClick={() => setShowFalse}>返回</div> */}
       </Container>
     </CSSTransition>
   );
