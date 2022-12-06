d0406fc090942c0f9ab71aeb57509338395825cf
SpringZz <wang90944@gmail.com>
2022-12-06 20:55:44 +0800

    修改redux错别字

--- src/application/Search/store/reducer.js
+++ src/application/Search/store/reducer.js
@@ -2,20 +2,20 @@ import * as actionTypes from './constants';
 import { fromJS } from 'immutable';
 
 const defaultState = fromJS({
-  hotList: [], // 热门关键词列表
-  suggestList: [], // 列表, 包括歌单和歌手
-  songList: [], // 歌曲列表
+  hotList: [],
+  suggestList: [],
+  songsList: [],
   enterLoading: false,
 });
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = defaultState, action) => {
   switch (action.type) {
-    case actionTypes.SET_HOT_KEYWORDS:
+    case actionTypes.SET_HOT_KEYWRODS:
       return state.set('hotList', action.data);
     case actionTypes.SET_SUGGEST_LIST:
       return state.set('suggestList', action.data);
-    case actionTypes.SET_RESULT_SONGS:
+    case actionTypes.SET_RESULT_SONGS_LIST:
       return state.set('songsList', action.data);
     case actionTypes.SET_ENTER_LOADING:
       return state.set('enterLoading', action.data);
