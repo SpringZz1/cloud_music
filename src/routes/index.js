1c35ea3d3ca5f0cbd458d442c0f081c9566c9c5e
SpringZz <wang90944@gmail.com>
2022-12-07 14:52:20 +0800

    添加懒加载路由，加快加载速度

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import Home from '../application/Home';
-import Recommend from '../application/Recommend';
-import Singers from '../application/Singers';
-import Rank from '../application/Rank';
-import Album from '../application/Album';
-import Singer from '../application/Singer';
-import Search from '../application/Search';
+const Recommend = React.lazy(() => import('../application/Recommend'));
+const Singers = React.lazy(() => import('../application/Singers'));
+const Rank = React.lazy(() => import('../application/Rank'));
+const Singer = React.lazy(() => import('../application/Singer'));
+const Album = React.lazy(() => import('../application/Album'));
+const Search = React.lazy(() => import('../application/Search'));
+// import Singers from '../application/Singers';
+// import Rank from '../application/Rank';
+// import Album from '../application/Album';
+// import Singer from '../application/Singer';
+// import Search from '../application/Search';
+// const RecommendComponent = React.lazy(() =>
+//   import('../application/Recommend/')
+// );
 
 const router = createBrowserRouter([
   {
@@ -21,27 +29,75 @@ const router = createBrowserRouter([
       },
       {
         path: '/recommend',
-        element: <Recommend />,
-        children: [{ path: ':id', element: <Album /> }],
+        // element: <Recommend />,
+        element: (
+          <React.Suspense>
+            <Recommend />
+          </React.Suspense>
+        ),
+        children: [
+          {
+            path: ':id',
+            element: (
+              <React.Suspense>
+                <Album />
+              </React.Suspense>
+            ),
+          },
+        ],
       },
       {
         path: '/singers',
-        element: <Singers />,
-        children: [{ path: '/singers/:id', element: <Singer /> }],
+        element: (
+          <React.Suspense>
+            <Singers />
+          </React.Suspense>
+        ),
+        children: [
+          {
+            path: '/singers/:id',
+            element: (
+              <React.Suspense>
+                <Singer />
+              </React.Suspense>
+            ),
+          },
+        ],
       },
       {
         path: 'rank',
-        element: <Rank />,
-        children: [{ path: ':id', element: <Album /> }],
+        element: (
+          <React.Suspense>
+            <Rank />
+          </React.Suspense>
+        ),
+        children: [
+          {
+            path: ':id',
+            element: (
+              <React.Suspense>
+                <Album />
+              </React.Suspense>
+            ),
+          },
+        ],
       },
       {
         path: 'search',
-        element: <Search />,
+        element: (
+          <React.Suspense>
+            <Search />
+          </React.Suspense>
+        ),
       },
       // 添加album路由, 用来显示歌单
       {
         path: '/album/:id',
-        element: <Album />
+        element: (
+          <React.Suspense>
+            <Album />
+          </React.Suspense>
+        ),
       },
     ],
   },
